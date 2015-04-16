2ddb26068846463faf3cf79ae3b36cdbeada52a7
Qiao Lin <qiaoclin@gmail.com>
2015-04-16 16:10:06 -0400

    user edit / tag search

--- server.js
+++ server.js
@@ -20,9 +20,18 @@ app.get('/', function(req,res){
 
 // redirecting search to document page 
 app.get('/documents/search', function(req, res){
-	db.get("SELECT id FROM documents WHERE title = ?", req.query.title, function (err, data){
+	db.get("SELECT id FROM documents WHERE title = ?", req.query.title.toUpperCase(), function (err, data){
 		if(err){throw err;}
 		res.redirect('/documents/' + data.id);
+	});
+});
+
+// render pages base off of tags 
+app.get('/tags/:type', function(req, res){
+	var type = req.params.type
+	db.all("SELECT * FROM documents WHERE tags = ?;", type, function(err, rows){
+		if(err){ throw err; }
+		res.render("tag_"+type+".ejs", {documents:rows}); 
 	});
 });
 
@@ -56,7 +65,7 @@ app.get("/documents/:id",function(req,res){
 //posting a new documents with filled content 
 app.post('/documents',function(req,res){
 	//this is where you would make any api request to insert in db
-	db.run("INSERT INTO documents (title, content, author_id) VALUES (?,?,?)", req.body.title, req.body.content, req.body.author, function(err){
+	db.run("INSERT INTO documents (title, content, author_id, image, tags) VALUES (?,?,?,?,?)", req.body.title.toUpperCase(), req.body.content, req.body.author, req.body.image, req.body.tags, function(err){
 		if(err){throw err; }
 		res.redirect('/documents') /// too tireed . . . .
 	});
@@ -81,7 +90,7 @@ app.put("/documents/:id", function(req,res){
 	var docID = parseInt(req.params.id);
 	var userID = req.body.user;
 	/// if requested any content, here would run request again for updates 
-	db.run("UPDATE documents SET title = ?, content = ?, image = ?, tags = ? WHERE id = ?", req.body.title, req.body.content, req.body.image, req.body.tags, docID, function(err){
+	db.run("UPDATE documents SET title = ?, content = ?, image = ?, tags = ? WHERE id = ?", req.body.title.toUpperCase(), req.body.content, req.body.image, req.body.tags, docID, function(err){
 		if(err){ throw err; }
 		db.run("INSERT INTO contribution (document_id, user_id, edit_summary) VALUES(?,?,?)", docID, userID, req.body.edit_summary, function(err){ 
 			if(err){ throw err; }
@@ -132,10 +141,38 @@ app.post('/users',function(req,res){
 	});
 });
 
+// render edit page for user update / 
+app.get('/users/:id/edit',function(req,res){
+	var userID = parseInt(req.params.id);
+	db.get("SELECT * FROM users where id = ?", userID, function(err, row){
+		if(err){ throw err; }
+		db.all("SELECT documents.id, documents.title FROM documents INNER JOIN subscription ON documents.id = subscription.document_id WHERE subscription.user_id = " +userID, function(err, data){
+			if(err){ throw err; }
+		res.render('user_edit.ejs', {user:row, subscription:data});
+		});
+	});
+});
+
+// update user profile while deleting subscription table 
+app.put("/users/:id", function(req,res){
+	var userID = parseInt(req.params.id);
+	var docID = req.body.doc;
+
+	db.run("UPDATE users SET name = ?, location = ?, pitch = ?, email_address = ? WHERE id = ?", req.body.name, req.body.location, req.body.pitch, req.body.email_address, userID, function(err){
+		if(err){ throw err; }
+		// probably not viable 
+		db.run("DELETE FROM subscription WHERE user_id = ? AND document_id = ? ", userID, docID, function(err){ 
+			if(err){ throw err; }
+			res.redirect('/users/' + userID);
+		});
+	});
+});
+
+
 // rendering contribution history log 
 app.get('/documents/:docID/contributions', function(req,res){
 	var docID = parseInt(req.params.docID);
-	db.all("SELECT * FROM contribution INNER JOIN users ON users.id = contribution.user_id WHERE contribution.document_id = ?", docID, function(err,data){
+	db.all("SELECT * FROM contribution INNER JOIN users ON users.id = contribution.user_id WHERE contribution.document_id = ? ORDER BY updated_at DESC", docID, function(err,data){
 		if(err){ throw err; }
 		res.render('contributions.ejs', {contribution:data});
 	});
@@ -144,47 +181,22 @@ app.get('/documents/:docID/contributions', function(req,res){
 //rendering a subscription page 
 app.get('/documents/:docID/subscribe',function(req, res){
 	var docID = parseInt(req.params.docID);
-	db.get("SELECT * FROM documents WHERE id = ?", docID, function(err, row){
+		db.get("SELECT * FROM documents WHERE id = ? ", docID, function(err, row){
+			if(err){ throw err; }
+	// how to find who's not on the subscription list 
+	db.all("SELECT users.id, users.name FROM users WHERE users.id NOT IN (SELECT user_id FROM subscription WHERE document_id = " +docID+")", function(err, notsubscribed){
 		if(err){ throw err; }
-		// search subscription to see who's subscribe 
-		db.all("SELECT users.id, users.name FROM users INNER JOIN subscription ON users.id = subscription.user_id WHERE subscription.document_id = " +docID, function(err, subscribed){
-				if(err){ throw err; }
-				console.log(subscribed);
-						//[ { id: 1, name: 'qatar Lion' },{ id: 3, name: 'gretchen' },{ id: 2, name: 'aung barto ' } ]
-		
-		//db.all("SELECT user_id FROM subscription WHERE document_id = ?", docID, function(err, info){
-			//  [ { user_id: 1 }, { user_id: 3 }, { user_id: 2 } ]
-
-			// how to find who's not on the subscription list 
-			db.all("SELECT users.id, users.name FROM users INNER JOIN subscription ON users.id != subscription.user_id WHERE subscription.document_id = " +docID, function(err, notsubscribed){
-				if(err){ throw err; }
-				console.log(notsubscribed);
-			});
-			
-
-
-			// then pass in people who haven't subscribed 
-			db.all("SELECT users.id, users.name FROM users WHERE user.id = ?", subscribed, function(err, data){
-			res.render('subscripe.ejs', {document:row, users:data});
-			});
+		res.render('subscribe.ejs', {document: row, users:notsubscribed});
 		});
-		
 	});
 });
 // inserting into subscription table user/document data
 
 app.post('/documents/:docID', function(req, res){ 
 	var docID = parseInt(req.params.docID);
-	db.get("SELECT * FROM subscription WHERE subscription.user_id = ? AND subscription.document_id = ?", req.body.user, docID, function(err,row){
+	db.run("INSERT INTO subscription (document_id, user_id) VALUES(?,?)", docID, req.body.user, function(err){
 		if(err){ throw err; }
-		console.log(row);
-	if(row === undefined){
-		db.run("INSERT INTO subscription (document_id, user_id) VALUES(?,?)", docID, req.body.user, function(err){
-			if(err){ throw err; }
-			res.redirect('/documents/'+ docID);
-		});
-	}else{ 
-		res.redirect('/documents/'+ docID); }
+		res.redirect('/documents/'+ docID);
 	});
 }); 
 
@@ -202,13 +214,27 @@ app.delete('/users/:id', function(req, res){
 	});
 });
 
-// //unsubscribing 
-// app.delete('/documents/:id/subscribe', function(req,res){
-// 	db.run('DELETE FROM subscription WHERE user_id ='+parseInt(req.body.user), function(err){
-// 		res.redirect('/documents/:id');
 
-// 	});
-// });
+//rendering a UN-subscription page 
+app.get('/documents/:docID/unsubscribe',function(req, res){
+	var docID = parseInt(req.params.docID);
+		db.get("SELECT * FROM documents WHERE id = ? ", docID, function(err, row){
+			if(err){ throw err; }
+	// search subscription to see who's already subscribe 
+		db.all("SELECT users.id, users.name FROM users INNER JOIN subscription ON users.id = subscription.user_id WHERE subscription.document_id = " +docID, function(err, subscribed){
+				if(err){ throw err; }
+		res.render('unsubscribe.ejs', {document: row, users:subscribed});
+		});
+	});
+});
+
+//unsubscribing 
+app.delete('/documents/:id/unsubscribe', function(req,res){
+	var docID = parseInt(req.params.id);
+	db.run('DELETE FROM subscription WHERE user_id ='+parseInt(req.body.user), function(err){
+		res.redirect('/documents/'+docID);
+	});
+});
 
 app.listen(3000, function(){
 	console.log("listening on" +3000);
